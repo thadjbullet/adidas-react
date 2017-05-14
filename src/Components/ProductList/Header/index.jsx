a4b97b56fc755c65b6ab7f6ead144abc3a28b1e2
Konstantin <thadjbullet@mail.ru>
2017-05-14 20:16:25 +0300

    Done with ProductList Header

--- src/Components/ProductList/Header/index.jsx
+++ src/Components/ProductList/Header/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import media from '../../../media';
+// import media from '../../../media';
 import Icon from './HeaderIcon';
+import Gender from './Gender';
+import Size from './Size';
 
 const Header = styled.header`
-  background: blue;
   border-bottom: 4px solid #e7e7e7;
   align-items: center;
   display: flex;
@@ -14,5 +15,7 @@ const Header = styled.header`
 export default () => (
   <Header>
     <Icon />
+    <Gender />
+    <Size />
   </Header>
 );
