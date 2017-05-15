f9efd73f9eb4e6319d6eb496f08068608c780b18
thadjbullet <thadjbullet@mail.ru>
2017-05-16 01:09:50 +0300

    fix: font, gradient, surge & redme (#4)

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ Build
 Lint check
 `yarn lint`
 
-[Link to Surge.sh](http://glamorous-dinner.surge.sh) - glamorous-dinner.surge.sh
+[Link to Surge.sh](http://repulsive-street.surge.sh) - repulsive-street.surge.sh
--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 export default styled.span`
-  color: white;
   background-color: #ff5c5c;
+  color: white;
+  font-family: 'AvenirBold';
   text-transform: uppercase;
   font-size: 14px;
-  padding: 7px 20px;
+  padding: 7px 20px 6px 21px;
 `;
